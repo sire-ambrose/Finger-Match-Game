e6f9896bd69eabee76eb85f39b240928040f4174
Sire Ambrose <ikpeleambroseobinna@gmail.com>
2021-04-10 00:42:21 +0100

    used instances intead of text file to transfer player choosen number from screen pickNum to screen game

--- game.py
+++ game.py
@@ -5,10 +5,19 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.image import Image
+from kivy.properties import NumericProperty
 from kivy.uix.button import ButtonBehavior
 from kivy.graphics import Color, Rectangle
+from kivy.event import EventDispatcher
+#help(EventDispatcher)
 import random
 from kivy.uix.screenmanager import ScreenManager, Screen
+'''
+ |                  >>> print(wid.property('sticks', quiet=True))
+ |                  None
+ |                  >>> wid.apply_property(sticks=ObjectProperty(55, max=10))
+-- More  --
+'''
 
 cdi={"0":Image(source="img0.jpg"),
 "1":Image(source="1c.gif"),
@@ -33,6 +42,8 @@ pdi={"0":Image(source="img0.jpg"),
 class win(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        #self.wpnum=plnum
+        #self.wi=i
         self.name='win'
         win=open('win.txt', 'r')
         rd=win.read()
@@ -43,8 +54,8 @@ class win(Screen):
         self.gd.add_widget(Button(text="New Game", on_press=self.gopickNum))
         self.gd.add_widget(Button(text="View Previous", on_press=self.previous))
     def gopickNum(self,instance):
-        	r.remove_widget(game())
-        	r.remove_widget(win())
+        	#r.remove_widget(game())
+        	#r.add_widget(pickNum())
         	self.manager.current='pickNum'
     def previous(self,instance):
         	r.remove_widget(win())
@@ -53,137 +64,9 @@ class win(Screen):
         
 #win=win()
 
-class pickNum(Screen):
-    def __init__(self, **kwargs):
-        super(pickNum, self).__init__(**kwargs)
-        self.mbox= BoxLayout(orientation='vertical')
-        self.add_widget(self.mbox)
-        self.mbox.add_widget(Label(text='Pick Your Number', size=(100,100)))
-        self.mgrid=GridLayout()
-        self.mgrid.cols=3
-        self.btn10=Button(text='10',on_press=self.gotofirst10)
-        self.mgrid.add_widget(self.btn10)
-        self.btn9=Button(text='9',on_press=self.gotofirst9)
-        self.mgrid.add_widget(self.btn9)
-        self.btn8=Button(text='8',on_press=self.gotofirst8)
-        self.mgrid.add_widget(self.btn8)
-        self.btn7=Button(text='7',on_press=self.gotofirst7)
-        self.mgrid.add_widget(self.btn7)
-        self.btn6=Button(text='6',on_press=self.gotofirst6)
-        self.mgrid.add_widget(self.btn6)
-        self.btn5=Button(text='5',on_press=self.gotofirst5)
-        self.mgrid.add_widget(self.btn5)
-        self.btn4=Button(text='4',on_press=self.gotofirst4)
-        self.mgrid.add_widget(self.btn4)
-        self.btn3=Button(text='3',on_press=self.gotofirst3)
-        self.mgrid.add_widget(self.btn3)
-        self.btn2=Button(text='2',on_press=self.gotofirst2)
-        self.mgrid.add_widget(self.btn2)
-        self.btn1=Button(text='1',on_press=self.gotofirst1)
-        self.mgrid.add_widget(self.btn1)
-        self.mbox.add_widget(self.mgrid)
-        self.btn0=Button(text='0',on_press=self.gotofirst0)
-        self.mgrid.add_widget(self.btn0)
-        self.btnn=Button(text='')
-        self.mgrid.add_widget(self.btnn)
-        self.name='pickNum'
-
-    def gotofirst10(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('10')
-        plnum.close()
-        self.pnum=open("plnum.txt", "r")
-        #r.remove_widget(welcome())
-        r.add_widget(game())
-        self.manager.current='game'
-    def gotofirst9(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('9')
-        plnum.close()
-        r.add_widget(game())
-        self.manager.current='game'
-    def gotofirst8(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('8')
-        plnum.close()
-        r.add_widget(game())
-        self.manager.current='game'
-    def gotofirst7(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('7')
-        plnum.close()
-        r.add_widget(game())
-        self.manager.current='game'
-    def gotofirst6(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('6')
-        plnum.close()
-        r.add_widget(game())
-        self.manager.current='game'
-    def gotofirst5(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('5')
-        plnum.close()
-        r.add_widget(game())
-        self.manager.current='game'
-    def gotofirst4(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('4')
-        plnum.close()
-        r.add_widget(game())
-        self.manager.current='game'
-    def gotofirst3(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('3')
-        plnum.close()
-        r.add_widget(game())
-        self.manager.current='game'
-    def gotofirst2(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('2')
-        plnum.close()
-        r.add_widget(game())
-        self.manager.current='game'
-    def gotofirst1(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('1')
-        plnum.close()
-        r.add_widget(game())
-        self.manager.current='game'
-            
-    def gotofirst0(self,instance):
-        plnum=open("plnum.txt", "w")
-        plnum.write('0')
-        plnum.close()
-        r.add_widget(game())
-        self.manager.current='game'
-class welcome(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.name='welcome'
-        #win=open('win.txt', 'r')
-        #rd=win.read()
-        #win.close()
-        self.add_widget(cover)
-        self.gd=FloatLayout()
-        self.add_widget(self.gd)
-        #self.gd.add_widget(Label(text=rd))
-        self.gd.add_widget(Button(text="Play", on_press=self.gopickNum, pos_hint={'x':0.5,'y':0.5},size_hint=(0.1,0.1)))
-        #self.gd.add_widget(Button(text="View Previous", on_press=self.previous))
-    def gopickNum(self,instance):
-        	r.add_widget(pickNum())
-        	#r.remove_widget(win())
-        	self.manager.current='pickNum'
-
-class manager(ScreenManager):
-    def __init__(self, **kwargs):
-        super(manager, self).__init__(**kwargs)
-        self.add_widget(welcome())
-       
-r=manager()
 class game(Screen):  
     
-    def __init__(self, **kwargs):
+    def __init__(self,pnum, **kwargs):
         super().__init__(**kwargs)
         self.name='game'
         self.bl=BoxLayout()
@@ -191,10 +74,8 @@ class game(Screen):
         self.bl.orientation="vertical"
         #self.add_widget(cover)
         self.add_widget(self.bl)
-		
-        self.pnum=int(open("plnum.txt", 'r').read())
-
-        #computer finger grid
+        self.pnum=pnum
+        #plnum=pnum
         self.cgrid=BoxLayout()
         self.cgrid.orientation="horizontal"
         self.bl.add_widget(self.cgrid)
@@ -285,7 +166,8 @@ class game(Screen):
             Color(1,0,1,0)
             Rectangle()
         #self.add_widget(self.pgrid)
-
+    def yyess(self, instance, plnum):
+        print('NO OnJHGF')
     def press0(self, *args):
         self.bl.remove_widget(self.pfin)
         self.pfin=pdi[str(0)]
@@ -305,6 +187,7 @@ class game(Screen):
             w.close()
             r.add_widget(win())
             self.manager.current='win'
+            print('GVDTKVDYYJ<GFKU')
         elif 0+self.fnum== self.cnum:
             w=open('win.txt', 'w')
             w.write('CPU\nWin')
@@ -446,8 +329,125 @@ class game(Screen):
             self.manager.current='win'
            
 
+class pickNum(Screen):
+    plnum=NumericProperty(12)
+    def __init__(self, **kwargs):
+        super(pickNum, self).__init__(**kwargs)
+        self.mbox= BoxLayout(orientation='vertical')
+        self.add_widget(self.mbox)
+        self.mbox.add_widget(Label(text='Pick Your Number'))
+        self.mgrid=GridLayout()
+        self.mgrid.cols=3
+        
+        self.btn10=Button(text='10',on_press=self.gotofirst10)
+        self.mgrid.add_widget(self.btn10)
+        self.btn9=Button(text='9',on_press=self.gotofirst9)
+        self.mgrid.add_widget(self.btn9)
+        self.btn8=Button(text='8',on_press=self.gotofirst8)
+        self.mgrid.add_widget(self.btn8)
+        self.btn7=Button(text='7',on_press=self.gotofirst7)
+        self.mgrid.add_widget(self.btn7)
+        self.btn6=Button(text='6',on_press=self.gotofirst6)
+        self.mgrid.add_widget(self.btn6)
+        self.btn5=Button(text='5',on_press=self.gotofirst5)
+        self.mgrid.add_widget(self.btn5)
+        self.btn4=Button(text='4',on_press=self.gotofirst4)
+        self.mgrid.add_widget(self.btn4)
+        self.btn3=Button(text='3',on_press=self.gotofirst3)
+        self.mgrid.add_widget(self.btn3)
+        self.btn2=Button(text='2',on_press=self.gotofirst2)
+        self.mgrid.add_widget(self.btn2)
+        self.btn1=Button(text='1',on_press=self.gotofirst1)
+        self.mgrid.add_widget(self.btn1)
+        self.mbox.add_widget(self.mgrid)
+        self.btn0=Button(text='0',on_press=self.gotofirst0)
+        self.mgrid.add_widget(self.btn0)
+        self.btnn=Button(text='')
+        self.mgrid.add_widget(self.btnn)
+        self.name='pickNum'
+
+    def gotofirst10(self,instance):
+        count=open('count.txt', 'r')
+        j=count.read()
+        count.close()
+        i=1+int(j)
+        print(i)
+        count=open('count.txt', 'w')
+        count.write(str(i))
+        count.close()
+        r.add_widget(game(pnum=10))
+        self.manager.current='game'
+    def gotofirst9(self,instance):
+        count=open('count.txt', 'r')
+        j=count.read()
+        count.close()
+        i=1+int(j)
+        print(i)
+        count=open('count.txt', 'w')
+        count.write(str(i))
+        count.close()
+        r.add_widget(game(pnum=9))
+        self.manager.current='game'
+    def gotofirst8(self,instance):
+        r.add_widget(game(pnum=8))
+        self.manager.current='game'
+    def gotofirst7(self,instance):
+        r.add_widget(game(pnum=7))
+        self.manager.current='game'
+    def gotofirst6(self,instance):
+        r.add_widget(game(pnum=6))
+        self.manager.current='game'
+    def gotofirst5(self,instance):
+        r.add_widget(game(pnum=5))
+        self.manager.current='game'
+    def gotofirst4(self,instance):
+        r.add_widget(game(pnum=4))
+        self.manager.current='game'
+    def gotofirst3(self,instance):
+        r.add_widget(game(pnum=3))
+        self.manager.current='game'
+    def gotofirst2(self,instance):
+        r.add_widget(game(pnum=2))
+        self.manager.current='game'
+    def gotofirst1(self,instance):
+        r.add_widget(game(pnum=1))
+        self.manager.current='game'
+    def gotofirst0(self,instance):
+        r.add_widget(game(pnum=0))
+        self.manager.current='game'
+    def on_plnum(self, instance, plnum):
+        print(plnum)
+        return plnum
+#pick=pickNum()
+class welcome(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name='welcome'
+        print(self.size)
+        self.add_widget(cover)
+        self.gd=FloatLayout()
+        self.add_widget(self.gd)
+        #self.gd.add_widget(Label(text=rd))
+        self.gd.add_widget(Button(text="Play", on_press=self.gopickNum, opacity=0.5, pos_hint={'x':0.5,'y':0.3},size_hint=(0.1,0.1)))
+        #self.gd.add_widget(Button(text="View Previous", on_press=self.previous))
+    def gopickNum(self,instance):
+        	r.add_widget(pickNum())
+        	#r.remove_widget(win())
+        	self.manager.current='pickNum'
+
+class manager(ScreenManager):
+    def __init__(self, **kwargs):
+        super(manager, self).__init__(**kwargs)
+        self.add_widget(welcome())
+       
+r=manager()
+
+
 class Kivy(App):
     def build(self):
+        count=open('count.txt', 'w')
+        count.write(str(0))
+        count.close()
         self.root = root=r
         root.bind(size=self._update_rect, pos=self._update_rect)
 
